c1748a970fc2679b66c1d8be08aed0b745873e11
João Filipe <joaofilipeb045@gmail.com>
2022-01-06 19:21:22 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,63 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 5;
+let highScore = 0;
+
+const setTextContent = function (elem, message) {
+    document.querySelector(elem).textContent = message;
+}
+
+const setDisableCheckAndInput = function () {
+    document.querySelector(".check").disabled = true;
+    document.querySelector(".guess").disabled = true;
+};
+
+const setEnableCheckAndInput = function () {
+    document.querySelector(".check").disabled = false;
+    document.querySelector(".guess").disabled = false;
+};
+
+ document.querySelector(".check").addEventListener("click", function () {
+    const guess = Number(document.querySelector(".guess").value);
+
+    if (!guess) {
+        setTextContent(".message", "⛔ No number!");
+    } else if (guess === secretNumber) {
+        setTextContent(".message", "🎉 Correct Number!");
+        setTextContent(".number", String(secretNumber));
+        document.querySelector("body").style.backgroundColor = "#60b347";
+        document.querySelector(".number").style.width = "30rem";
+
+        if (score > highScore) {
+            highScore = score;
+            setTextContent(".highscore", String(highScore));
+        }
+
+        setDisableCheckAndInput();
+    } else {
+        if (score > 1) {
+            setTextContent(".message", guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
+            score--;
+            setTextContent(".score", String(score));
+        } else {
+            setTextContent(".number", String(secretNumber));
+            setTextContent(".message", "💥 You lost the game!");
+            setTextContent(".score", String(0));
+            document.querySelector("body").style.backgroundColor = "#ce1313";
+            setDisableCheckAndInput();
+        }
+    }
+});
+
+ document.querySelector(".again").addEventListener("click", function () {
+   score = 5;
+   secretNumber = Math.trunc(Math.random() * 20) + 1;
+   setEnableCheckAndInput();
+   setTextContent(".message", "Start guessing...");
+   setTextContent(".number", "?");
+   document.querySelector(".score").textContent = String(score);
+   document.querySelector(".guess").value = "";
+   document.querySelector("body").style.backgroundColor = "#222";
+   document.querySelector(".number").style.width = "15rem";
+});
